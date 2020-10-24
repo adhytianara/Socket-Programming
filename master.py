53562cbf5d17343835ba35eb17ac54d8c563abf9
Ronaldi Tjaidianto <ronalditjaidianto01@gmail.com>
2020-10-24 23:47:47 +0800

    add more jobs

--- master.py
+++ master.py
@@ -1,12 +1,23 @@
 import socket
 import pickle
+import time
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+print("Commands:")
+print("[1] List sorting. How to use: sort [list]")
+print("[2] Book search. How to use: book_search \"Harry_potter\". Note: book title in quotes and separated with underscores (_)")
+print("[3] Generate n-th fibonnaci number. How to use: fibonacci n")
+
+command = input("Input command:")
+
+start = time.time()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'sort [5,7,4,3,9,10]')
+    s.sendall(bytes(command, "utf-8"))
     data = s.recv(1024)
+end = time.time()
 
-print('Received', data.decode("utf-8"))
+print('RESULT:\n{}'.format(data.decode("utf-8")))
+print("Time elapsed:", end-start)
--- worker.py
+++ worker.py
@@ -1,5 +1,6 @@
 import socket
 import pickle
+import requests
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -14,9 +15,32 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
+            
             command_args = data.decode("utf-8").split(" ")
             if command_args[0] == "sort":
                 lst_arg = eval(command_args[1])
                 lst_arg.sort()
                 result = "Sorted list: " + str(lst_arg)
+            elif command_args[0] == "book_search":
+                book_arg = command_args[1].split("\"")[1]
+                r = requests.get('https://www.googleapis.com/books/v1/volumes?q=' + book_arg.replace("_", "%20"))
+                r_json = r.json()
+                result = "Buku ditemukan: {}\n".format(r_json["totalItems"])
+
+                limit = 15
+                if len(r_json["items"]) < 15:
+                    limit = len(r_json["items"])
+                for i in range(0,limit):
+                    info_buku = r_json["items"][i]["volumeInfo"]
+                    result += "[{}] {}\n".format(i+1, info_buku["title"])
+            elif command_args[0] == "fibonacci":
+                fib_num = eval(command_args[1])
+
+                def fibonacci(n):
+                    if n <= 1:
+                        return n
+                    return fibonacci(n-1) + fibonacci(n-2)
+                    
+                
+                result = "Fibonacci ke-{}: {}".format(fib_num, fibonacci(fib_num))
             conn.sendall(bytes(result, "utf-8"))
