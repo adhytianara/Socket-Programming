b64185e9b79a790d74185db1ffcd03f6693abc7d
Adhytia <adit.14.12.99@gmail.com>
2020-10-25 16:02:11 +0700

    fixed some issues and refactor the code

--- master.py
+++ master.py
@@ -1,23 +1,33 @@
 import socket
-import pickle
 import time
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-print("Commands:")
-print("[1] List sorting. How to use: sort [list]")
-print("[2] Book search. How to use: book_search \"Harry_potter\". Note: book title in quotes and separated with underscores (_)")
-print("[3] Generate n-th fibonnaci number. How to use: fibonacci n")
+def main():
+    while True:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST, PORT))
+        
+        command = print_list_of_commands()
+        client.sendall(bytes(command, "utf-8"))
 
-command = input("Input command:")
+        data = client.recv(1024).decode('utf-8')
+        if not data:
+            break
+        print(f'{data}\n')
 
-start = time.time()
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(bytes(command, "utf-8"))
-    data = s.recv(1024)
-end = time.time()
 
-print('RESULT:\n{}'.format(data.decode("utf-8")))
-print("Time elapsed:", end-start)
+
+def print_list_of_commands():
+    print('\n-------------------- Waiting for new commands --------------------')
+    print("List of Commands:")
+    print("[1] List sorting. How to use: sort [list]")
+    print("[2] Book search. How to use: book_search \"Harry_potter\". Note: book title in quotes and separated with underscores (_)")
+    print("[3] Generate n-th fibonnaci number. How to use: fibonacci n")
+    command = input("Input command: ")
+    return command
+
+
+if __name__ == '__main__':
+    main()
